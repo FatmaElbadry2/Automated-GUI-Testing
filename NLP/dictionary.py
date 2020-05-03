5da12351afab156cbdf54e5ba15670ae90ffecc5
FatmaElbadry <you@example.com>
2020-05-04 00:15:54 +0200

    dictionary added

---
+++ NLP/dictionary.py
@@ -0,0 +1,109 @@
+verbs =\
+[
+    "click",
+    "push",
+    "press",
+    "hit",
+    "add",
+    "open",
+    "close",
+    "go to",
+    "tap",
+    "tick",
+    "save",
+    "submit",
+    "load",
+    "cut",
+    "copy",
+    "paste",
+    "load",
+    "undo",
+    "redo",
+    "export",
+    "new",
+    "search",
+    "scroll up",
+    "scroll down",
+    "return",
+    "back",
+    "switch on",
+    "switch off",
+    "refresh",
+    "write",
+    "highlight",
+    "type",
+    "enter",
+    "compose",
+    "run",
+    "stop",
+    "drag",
+    "hold",
+    "drop",
+    "move",
+    "check",
+    "select",
+    "choose",
+    "pick",
+    "assign",
+    "point",
+    "fix",
+    "reset",
+    "set",
+    "delete",
+    "remove",
+    "pin",
+    "attach",
+    "switch",
+    "browse"
+]
+
+objects =\
+    [
+        "cursor",
+        "home",
+        "option",
+        "object",
+        "target",
+        "screen",
+        "window",
+        "program",
+        "application",
+        "app",
+        "mouse",
+        "keyboard",
+        "file",
+        "directory"
+    ]
+
+adjectives =\
+[
+    "light",
+    "dark",
+    "top",
+    "bottom",
+    "middle",
+    "left",
+    "right",
+    "up",
+    "down"
+]
+
+colors = {
+    "red": red,
+    "green": [0x001a00,0xccffcc],
+    "blue": [],
+    "yellow": [],
+    "purple": [],
+    "violet": [],
+    "orange": [],
+    "pink": [],
+    "brown": [],
+    "black": [],
+    "white": [],
+    "grey": [],
+}
+
+
+
+
+
