309a6394cb30a76ccc3b4953f1edff62cf395395
ranaibrahim <rana.ibrahimhassan@yahoo.com>
2020-05-12 00:58:35 +0200

    check crashes

--- Shortcuts.py
+++ Shortcuts.py
@@ -3,10 +3,26 @@ import pyscreenshot as ImageGrab
 import pyperclip  # handy cross-platform clipboard text handler
 import time
 import subprocess
+import win32gui
 
-def OpenApp(PATH,APPNAME):
+def OpenApp(PATH,APPNAME): #APPName with extention
+    x = (PATH + "\\" + APPNAME)
+    raw_string = r"{}".format(x)
+    subprocess.Popen(raw_string)
 
-    subprocess.Popen([PATH+'\\'+APPNAME, '-nc'])
+def IsRunning(ProgramName): #program name without extention
+   if win32gui.FindWindow(None, ProgramName):
+      return True
+   else:
+      return False
+
+def IsOpen(PATH, APPNAME,APPWINDOW): #APPNAME=APPNAME.exe   APPWINDOW=APPNAME without ext.
+    OpenApp(PATH,APPNAME)
+    x=IsRunning(APPWINDOW)
+    while(x!=True):
+        print("waiting")
+        time.sleep(1)
+        x = IsRunning(APPWINDOW)
 
 def Cut():
     pyautogui.hotkey('ctrl','x')
