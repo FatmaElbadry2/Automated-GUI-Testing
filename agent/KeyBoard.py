3a84753781b9ba88faf87d38542e4c749f191048
FatmaElbadry <you@example.com>
2020-06-07 20:50:31 +0200

    Update KeyBoard.py

--- agent/KeyBoard.py
+++ agent/KeyBoard.py
@@ -1,19 +1,25 @@
 from agent.imports import *
 
+
 def Write(Word):
     pyautogui.write(Word, interval=0.25)
+
 
 def WriteLetters():
     pyautogui.write("abCD", interval=0.25)
 
+
 def WriteNumbers():
     pyautogui.write(1234, interval=0.25)
+
 
 def WriteSpecialCha():
     pyautogui.write('_', interval=0.25)
 
+
 def TwoKeys(FirstKey,SecondKey):
     pyautogui.hotkey(FirstKey,SecondKey)
+
 
 def KeyPress(Key):
     pyautogui.press(Key)
@@ -21,6 +27,7 @@ def KeyPress(Key):
 
 def ThreeKeys(FirstKey,SecondKey,ThirdKey):
     pyautogui.hotkey(FirstKey,SecondKey,ThirdKey)
+
 
 def pressArrow(Arrow,numberofpresses):
     pyautogui.press(Arrow, presses=numberofpresses)
